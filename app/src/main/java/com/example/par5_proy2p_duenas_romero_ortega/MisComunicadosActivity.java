bb040691a147b1b602fb0eb0e71ca3625f7ee5ef
WSmithDR <wsduenas@espol.edu.ec>
2025-08-11 18:19:18 -0500

    feat: creada la vista para el tablero de comunicados

--- app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/MisComunicadosActivity.java
+++ app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/MisComunicadosActivity.java
@@ -11,21 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MisComunicadosActivity extends AppCompatActivity {
-    private TableLayout tablaMisComunicados;
+    /*private TableLayout tablaMisComunicados;
     private Button btnOrdernarTitulo;
     private Button btnGuardarLista;
-    private Button btnVolver;
+    private Button btnVolver;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mis_comunicados);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
     }
 
     public void ordenarPorTitulo(){}
