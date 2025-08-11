ac30bd261899a85011b237c44a136f5c88177516
OrtegaDanilo <dancamac@espol.edu.ec>
2025-08-11 13:52:18 -0500

    Implementación de los botones del MainActivity: Ver Comunicados, Publicar Comunicado y Tablero Comunicados, con sus respectivo setOnClickListener.

---
+++ app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.par5_proy2p_duenas_romero_ortega;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup editBtn_grupo;
+    private Button editBtn_ver;
+    private Button editBtn_publicar;
+    private Button editBtn_tablero;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textoprueba), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editBtn_grupo = findViewById(R.id.btn_grupo);
+        editBtn_ver = findViewById(R.id.btn_verComunicados);
+        editBtn_publicar = findViewById(R.id.btn_publicarComunicados);
+        editBtn_tablero = findViewById(R.id.btn_tablero);
+
+        editBtn_ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, VerComunicadosActivity.class));
+            }
+        });
+
+        editBtn_publicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PublicarComunicadoActivity.class));
+            }
+        });
+
+        editBtn_tablero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MisComunicadosActivity.class));
+            }
+        });
+    }
+}
