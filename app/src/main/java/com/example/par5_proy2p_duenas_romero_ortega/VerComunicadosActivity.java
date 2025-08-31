1b473197f384f91ad1c3f858b9a61a1dc58e9281
OrtegaDanilo <dancamac@espol.edu.ec>
2025-08-31 11:18:44 -0500

    Volver a la pantalla principal desde VerComunicadosActivity.

--- app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/VerComunicadosActivity.java
+++ app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/VerComunicadosActivity.java
@@ -1,6 +1,10 @@
 package com.example.par5_proy2p_duenas_romero_ortega;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class VerComunicadosActivity extends AppCompatActivity {
+    private Button btn_selFecha;
+    private TextView selFecha;
+    private Button btn_volverVerCom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,18 @@ public class VerComunicadosActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btn_selFecha = findViewById(R.id.Btn_selFecha);
+        selFecha = findViewById(R.id.txtFecha);
+        btn_volverVerCom = findViewById(R.id.Btn_volverCom);
+
+        //Volver al activity anterior
+        btn_volverVerCom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VerComunicadosActivity.this, MainActivity.class));
+            }
         });
     }
 }
