a6804984e92754ca4f2c9bb10f0346fdd4beef9d
OrtegaDanilo <dancamac@espol.edu.ec>
2025-08-31 11:56:48 -0500

    Se añadió la funcionalidad de selección de fecha mediante un DatePickerDialog y se actualizó el texto del botón de retorno en la interfaz.

--- app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/VerComunicadosActivity.java
+++ app/src/main/java/com/example/par5_proy2p_duenas_romero_ortega/VerComunicadosActivity.java
@@ -1,5 +1,6 @@
 package com.example.par5_proy2p_duenas_romero_ortega;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class VerComunicadosActivity extends AppCompatActivity {
     private Button btn_selFecha;
@@ -40,5 +43,21 @@ public class VerComunicadosActivity extends AppCompatActivity {
                 startActivity(new Intent(VerComunicadosActivity.this, MainActivity.class));
             }
         });
+
+        //Seleccionar fecha
+        btn_selFecha.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    (view, y, m, d) -> {
+                        String fecha = String.format("%02d/%02d/%04d", d, m+1, y);
+                        selFecha.setText(fecha);
+                    }, year, month, day);
+            datePickerDialog.show();
+        });
+
     }
 }
