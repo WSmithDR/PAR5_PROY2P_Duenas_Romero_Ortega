e121efc9f23cb9858f99b0ccc3eaba2566688663
Abraham_Romero <abrarome@espol.edu.ec>
2025-09-02 22:31:46 -0500

    Se realizo el javadoc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(fileTree(mapOf(
+        "dir" to "C:\\Users\\PC\\AppData\\Local\\Android\\Sdk\\platforms\\android-36",
+        "include" to listOf("*.aar", "*.jar"),
+        "exclude" to listOf()
+    )))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
